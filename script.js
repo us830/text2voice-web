3d78109034f13ccab7960f0c267923ac594604b6
us830 <3q@kcapsys.com>
2024-09-18 13:41:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     "voice-api": {
-        url: "https://ttsapi.zwei.de.eu.org/tts",
+        url: "https://text2sppch.fme.us.kg/tts",
         speakers: {
             "zh-CN-XiaoxiaoNeural": "晓晓",
             "zh-CN-YunxiNeural": "云希",
@@ -118,7 +118,7 @@ function generateVoice(isPreview) {
         url: url,
         method: 'GET',
         headers: {
-            'x-api-key': '@ak47'  // 添加 API 密钥
+            'x-api-key': 'skyxin'  // 添加 API 密钥
         },
         xhrFields: {
             responseType: 'blob' // 确保返回的是一个Blob对象
